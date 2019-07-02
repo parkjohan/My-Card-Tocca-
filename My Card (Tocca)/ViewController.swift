cb6eefb9409d34ccc37bc06b427fd3130980b5d0
Johan Park <parkjohan@gmail.com>
2019-07-01 21:03:36 -0700

    Initial Commit

---
+++ My Card (Tocca)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Card (Tocca)
+//
+//  Created by Johan Park on 7/1/19.
+//  Copyright © 2019 Johan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
